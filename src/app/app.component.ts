808f8e6ebe51ddaf7cbc4ecc217191ccfdff4461
amoose20 <amussey3@gmail.com>
2023-01-24 09:30:34 -0600

    Functionality is working. Styling still WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    oddNumbers: number[] = [];
+    evenNumbers: number[] = [];
+
     onGameStarted(firedNumber: number) {
-        console.log(firedNumber)
+        if (firedNumber % 2 === 0) {
+            this.evenNumbers.push(firedNumber);
+        } else {
+            this.oddNumbers.push(firedNumber);
+        }
     }
 }
--- src/app/odd/odd.component.ts
+++ src/app/odd/odd.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./odd.component.css']
 })
 export class OddComponent {
+    @Input() number: number;
 
 }
